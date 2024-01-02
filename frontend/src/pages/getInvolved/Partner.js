3bddc31e2be89ab6992f3dedc62cc98638f8bef9
vishal-2023 <vishalsingh23.it@gmail.com>
2024-01-02 15:26:29 +0530

    second commit

--- frontend/src/pages/getInvolved/Partner.js
+++ frontend/src/pages/getInvolved/Partner.js
@@ -9,7 +9,8 @@ const Partner = () => {
         <Navbar/>
         <img className='w-full h-80 scale-100 mt-2 ' src='https://goonj.org/wp-content/uploads/2018/05/partner-Ban-977x650.png' alt='' />
         <div className='absolute top-40 w-full mx-auto flex items-center justify-center   text-white text-3xl leading-9 font-medium'>Partner with us</div>  
-
+        <div className='absolute top-40 w-full mx-auto flex items-center justify-center   text-white text-3xl leading-9 font-medium'>Partner with us</div>  
+        
         <Share/>
         <Footer/>
     </div>
