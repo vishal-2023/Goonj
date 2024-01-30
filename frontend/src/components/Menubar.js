2af4700c88bc002d56a82ac0b8b0a31ff5a4659c
vishal-2023 <vishalsingh23.it@gmail.com>
2024-01-30 22:47:38 +0530

    media query added sucessfully

--- frontend/src/components/Menubar.js
+++ frontend/src/components/Menubar.js
@@ -4,14 +4,14 @@ import { NavLink } from 'react-router-dom';
 const Menubar = ({dropDown}) => {
     
   return (
-    <ul className='relative m-5 z-50 md:right-6'>
+    <ul className='relative m-5 z-50 right-9 md:right-6'>
         {
-            <li className='absolute  w-40 mt-5  flex flex-col  z-50 bg-slate-200 border-2'>
+            <li className='absolute w-32 md:w-40 md:mt-5  flex flex-col  z-50 bg-slate-200 border-2'>
             {
                 dropDown.map((item) => {
-                return <li className='p-2  z-50 hover:bg-slate-100 text-base font-semibold' >
+                return <li className='md:p-2   z-50 hover:bg-slate-100 text-base font-semibold' >
                     {    
-                    <NavLink className='' to={item.path} key={item.id}>      
+                    <NavLink className='text-xs md:text-sm' to={item.path} key={item.id}>      
                         {item.title}
                     </NavLink>
                     }  
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,17 +7,9 @@ import { useState } from 'react';
 
 
 const NavBox= () =>{
-  <div className='border-4 top-20 bg-pink-600 w-full h-full'>
+  <div className='border-4  bg-pink-600 w-full h-full'>
     console.log("first");
-    <ul className='flex flex-col gap-2'>
-      <li>About us</li>
-      <li>Impact</li>
-      <li>Get Involved</li>
-      <li>Updates</li>
-      <li>Contribute Material</li>
-      <li>Contact us</li>
-
-    </ul>
+    
   </div>
 }
 
@@ -68,115 +60,178 @@ export const Navbar = () => {
     fourIsHovering(false);
   };
 
-  const [isNavBoxVisible, setNavBoxVisible] = useState(true);
+  const [isHidden, setIsHidden] = useState(false);
 
-  const toggleNavBoxVisibility = () => {
-    setNavBoxVisible(!isNavBoxVisible);
+  const handleClick = () => {
+    // Toggle the value of isHidden
+    setIsHidden(!isHidden);
   };
+
     
     
 
   
   return (
-    <div className='w-full z-50 fixed bg-white border-b  p-1'>
-    <div className=' box-border h-12 text-sm  w-12/13 p-4 border-0 flex flex-row items-center mx-auto justify-between'>
-      <div className=''>
-        <NavLink to="/">
-          <img className=' w-24 min-[375px]:w-20 h-full ' src='https://goonj.org/wp-content/uploads/2020/06/Goonj-logo-10June20.png' alt='img' />
-        </NavLink>
-      </div>
-      <div className='hidden md:block'>
-        <ul className='flex flex-row items-center  z-40 justify-center text-sm font-semibold  gap-6'>
-          <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>  
-            
-            <NavLink className='block '>About us</NavLink>
-            {firHovering && (
-              <div className='top-5 h-full z-50 absolute '>
-                <Menubar  dropDown={AboutdropDown} />
-              </div>)  
-            }    
-          </li>
-          <li className='relative h-16 te flex flex-col items-center justify-center' onMouseEnter={handleMouseOver1} onMouseLeave={handleMouseOut1}>
-            <NavLink className=' '> Impact </NavLink>
-            {secHovering && (
-              <div className='top-5 absolute'>
-              <Menubar  dropDown={impactdropDown} />
-            </div>  
-            )
-            }
-            
-          </li>
-          <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver2} onMouseLeave={handleMouseOut2}>
-            <NavLink className=''>Get Involved</NavLink>
-            {
-            fifthHovering && (
-              <div className='top-5 right-9 absolute'>
-              <Menubar  dropDown={getdropDown} />
-            </div>  
-            ) 
-            }
-          </li>
-          <li className='relative h-16 flex flex-col z-50 items-center justify-center' onMouseEnter={handleMouseOver3} onMouseLeave={handleMouseOut3}>
-            <NavLink className=''>Updates</NavLink>
-            {
-              thirdHovering && (
-                <div className='top-5 z-50 absolute'>
-              <Menubar  dropDown={updatadropDown} />
-            </div>
-              )
-            }
-          </li>
-          <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver4} onMouseLeave={handleMouseOut4}>
-            <NavLink className=''>Contribute Material</NavLink>
-            {
-              fourHovering && (
-                <div className='top-5 right-16 absolute'>
-              <Menubar  dropDown={contributedropDown} />
-            </div>
-              )
-            }
-          </li>
-          <li className=''>
-            <NavLink to="/our-offices/">Contact us</NavLink>
-          </li>
-        </ul>
-      </div>
-      
-      <div className='flex flex-row gap-3 items-center justify-center'>
-        <div>
-          <NavLink> <FaSearch/> </NavLink>
-        </div>
-        <div>
-          <NavLink to="https://www.facebook.com/goonj.org"> <FaFacebook/> </NavLink>
-        </div>
-        <div>  
-          <NavLink to="https://www.instagram.com/goonj/"> <FaInstagram/> </NavLink>
-        </div>
-        <div> 
-          <NavLink to="https://twitter.com/goonj"> <FaTwitter/> </NavLink>
-        </div>
-        <div>  
-        <NavLink to="https://www.youtube.com/channel/UCCq8iYlmjT7rrgPI1VHzIHg"> <FaYoutube/> </NavLink>
-        </div>
-
-
-        <div className='flex flex-row gap-3 items-center justify-center'>
-          {/* Mobile Menu Icon */}
-          <div className='' onClick={toggleNavBoxVisibility}>
-            <FaThList />
+    
+    <div className='flex flex-col bg-white w-full h-full'>
+      <div className='w-full z-50 fixed bg-inherit border-b  p-1'>
+        <div className=' box-border md:h-12 text-sm  w-12/13 md:p-4 border-0 flex flex-row items-center mx-auto justify-between'>
+          <div className=''>
+            <NavLink to="/">
+              <img className=' w-24 min-[375px]:w-20 h-full ' src='https://goonj.org/wp-content/uploads/2020/06/Goonj-logo-10June20.png' alt='img' />
+            </NavLink>
           </div>
+          <div className='hidden md:block'>
+            <ul className='flex flex-row items-center  z-40 justify-center text-sm font-semibold  gap-6'>
+              <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>  
+                
+                <NavLink className='block '>About us</NavLink>
+                {firHovering && (
+                  <div className='top-5 h-full z-50 absolute '>
+                    <Menubar  dropDown={AboutdropDown} />
+                  </div>)  
+                }    
+              </li>
+              <li className='relative h-16 te flex flex-col items-center justify-center' onMouseEnter={handleMouseOver1} onMouseLeave={handleMouseOut1}>
+                <NavLink className=' '> Impact </NavLink>
+                {secHovering && (
+                  <div className='top-5 absolute'>
+                  <Menubar  dropDown={impactdropDown} />
+                </div>  
+                )
+                }
+                
+              </li>
+              <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver2} onMouseLeave={handleMouseOut2}>
+                <NavLink className=''>Get Involved</NavLink>
+                {
+                fifthHovering && (
+                  <div className='top-5 right-9 absolute'>
+                  <Menubar  dropDown={getdropDown} />
+                </div>  
+                ) 
+                }
+              </li>
+              <li className='relative h-16 flex flex-col z-50 items-center justify-center' onMouseEnter={handleMouseOver3} onMouseLeave={handleMouseOut3}>
+                <NavLink className=''>Updates</NavLink>
+                {
+                  thirdHovering && (
+                    <div className='top-5 z-50 absolute'>
+                  <Menubar  dropDown={updatadropDown} />
+                </div>
+                  )
+                }
+              </li>
+              <li className='relative h-16 flex flex-col items-center justify-center' onMouseEnter={handleMouseOver4} onMouseLeave={handleMouseOut4}>
+                <NavLink className=''>Contribute Material</NavLink>
+                {
+                  fourHovering && (
+                    <div className='top-5 right-16 absolute'>
+                  <Menubar  dropDown={contributedropDown} />
+                </div>
+                  )
+                }
+              </li>
+              <li className=''>
+                <NavLink to="/our-offices/">Contact us</NavLink>
+              </li>
+            </ul>
+          </div>
+          
+          <div className='flex flex-row gap-3 items-center justify-center'>
+            <div>
+              <NavLink> <FaSearch/> </NavLink>
+            </div>
+            <div>
+              <NavLink to="https://www.facebook.com/goonj.org"> <FaFacebook/> </NavLink>
+            </div>
+            <div>  
+              <NavLink to="https://www.instagram.com/goonj/"> <FaInstagram/> </NavLink>
+            </div>
+            <div> 
+              <NavLink to="https://twitter.com/goonj"> <FaTwitter/> </NavLink>
+            </div>
+            <div>  
+            <NavLink to="https://www.youtube.com/channel/UCCq8iYlmjT7rrgPI1VHzIHg"> <FaYoutube/> </NavLink>
+            </div>
 
-          {/* Render NavBox if isNavBoxVisible is true */}
-          {isNavBoxVisible && <NavBox />}
+
+            <div className='flex flex-row gap-3 items-center justify-center'>
+              {/* Mobile Menu Icon */}
+              <div className='' >
+                <FaThList onClick={handleClick}/>
+              </div>
+
+              {/* Render NavBox if isNavBoxVisible is true */}
+              { <NavBox />}
+            </div>
+
+            
+            <NavLink className='hidden md:block ' to="/donate/">
+              <button class="bg-red-500 py-2 rounded-md px-3 text-white font-semibold text-base">CONTRIBUTE</button>
+            </NavLink>
+            
+          </div>
         </div>
-
-        
-        <NavLink className='hidden md:block ' to="/donate/">
-          <button class="bg-red-500 py-2 rounded-md px-3 text-white font-semibold text-base">CONTRIBUTE</button>
-        </NavLink>
-        
       </div>
-    </div>
+      {isHidden &&
+      <div className='border-2 md:hidden mt-16 md:h-16'>
+      <ul className='flex flex-row lg:hidden flex-wrap items-center  z-40 justify-center text-sm font-semibold  gap-2'>
+              <li className='relative border-2  flex flex-col ' onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>  
+                
+                <NavLink className='text-xs '>About us</NavLink>
+                {firHovering && (
+                  <div className='ml-4 h-full z-50 absolute '>
+                    <Menubar  dropDown={AboutdropDown} />
+                  </div>)  
+                }    
+              </li>
+              <li className='relative   flex flex-col items-center justify-center' onMouseEnter={handleMouseOver1} onMouseLeave={handleMouseOut1}>
+                <NavLink className='text-xs '> Impact </NavLink>
+                {secHovering && (
+                  <div className=' absolute'>
+                  <Menubar  dropDown={impactdropDown} />
+                </div>  
+                )
+                }
+                
+              </li>
+              <li className='relative  flex flex-col items-center justify-center' onMouseEnter={handleMouseOver2} onMouseLeave={handleMouseOut2}>
+                <NavLink className='text-xs'>Get Involved</NavLink>
+                {
+                fifthHovering && (
+                  <div className=' right-9 absolute'>
+                  <Menubar  dropDown={getdropDown} />
+                </div>  
+                ) 
+                }
+              </li>
+              <li className='relative flex flex-col z-50 items-center justify-center' onMouseEnter={handleMouseOver3} onMouseLeave={handleMouseOut3}>
+                <NavLink className='text-xs'>Updates</NavLink>
+                {
+                  thirdHovering && (
+                    <div className='right-3  absolute'>
+                  <Menubar  dropDown={updatadropDown} />
+                </div>
+                  )
+                }
+              </li>
+              <li className='relative flex flex-col items-center justify-center' onMouseEnter={handleMouseOver4} onMouseLeave={handleMouseOut4}>
+                <NavLink className='text-xs'>Contribute Material</NavLink>
+                {
+                  fourHovering && (
+                    <div className=' absolute'>
+                  <Menubar  dropDown={contributedropDown} />
+                </div>
+                  )
+                }
+              </li>
+              <li className='text-xs'>
+                <NavLink to="/our-offices/">Contact us</NavLink>
+              </li>
+            </ul>
+      </div>
+      }
     </div>
   )
 }
